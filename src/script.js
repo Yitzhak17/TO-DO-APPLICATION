96c24b86bc19bcbd625a0c9e7d8c622cc86e3b94
Yitzhak Keidar <keidar17@gmail.com>
2025-09-19 09:29:37 +0300

    feat: add toggle functionality for todo items
    - Added toggleTodoItem function
    - Updated createTodoComponents to attach toggle button
    - Display date and time using toLocaleDateString + toLocaleTimeString

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 let todos = [
   { id: 1, text: "Go to buy some milk", created: Date.now(), isCompleted: false },
 
-  { id: 1, text: "Go to buy some milk", created: Date.now(), isCompleted: false },
+  { id: 2, text: "Go to buy some milk", created: Date.now(), isCompleted: false },
 
-  { id: 1, text: "Go to buy some milk", created: Date.now(), isCompleted: false },
+  { id: 3, text: "Go to buy some milk", created: Date.now(), isCompleted: false },
 
-  { id: 1, text: "Go to buy some milk", created: Date.now(), isCompleted: true }
+  { id: 4, text: "Go to buy some milk", created: Date.now(), isCompleted: true }
 ]
 
 const todoListElement = document.getElementById("to-do-list")
@@ -19,15 +19,24 @@ const renderTodoList = () => {
   todoListElement.innerHTML = todoComponets
 }
 
-const createTodoCompenets = (todo) => {
-  const date = new Date(todo.created).toLocaleString('he-IL')
+const createTodoCompenents = (todo) => {
+  const date = new Date(todo.created).toLocaleDateString('he-IL')
+  const time = new Date(todo.created).toLocaleTimeString('he-IL')
+
   return `
       <figure>
         <h3 class="${todo.isCompleted ? "completed" : ''}" >${todo.text}</h3>
-        <p class="${todo.isCompleted ? "completed" : ''}">${date}</p>
-        <button class="toggle-todo">${todo.isCompleted ? `Completed` : `Checked`}</button>
+        <p class="${todo.isCompleted ? "completed" : ''}">${date} ${time}</p>
+        <button onclick="toggleTodoItem(${todo.id})" class="toggle-todo">${todo.isCompleted ? `Completed` : `Checked`}</button>
         <div class="remove-todo">X</div>
       </figure>`
 
+}
+
+const toggleTodoItem = (id) => {
+  const idx = todos.findIndex(todo => todo.id === id)
+  console.log(`the todo taht was clicked:`, idx);
+  todos[idx].isCompleted = !todos[idx].isCompleted;
+  renderTodoList();
 }
 renderTodoList();
